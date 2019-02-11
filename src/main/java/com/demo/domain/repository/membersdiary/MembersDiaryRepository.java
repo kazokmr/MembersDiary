2a0c0899c414ad7de6f5dbfcd16b56ae4982663c
Kazuhiko Okumura <engkokmr@gmail.com>
2019-02-11 10:15:15 +0900

    MembersDiaryRepositoryインターフェースの@Repositoryを除去

--- src/main/java/com/demo/domain/repository/membersdiary/MembersDiaryRepository.java
+++ src/main/java/com/demo/domain/repository/membersdiary/MembersDiaryRepository.java
@@ -2,8 +2,6 @@ package com.demo.domain.repository.membersdiary;
 
 import com.demo.domain.entity.membersdiary.MembersDiary;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MembersDiaryRepository extends CrudRepository<MembersDiary, Integer> {
 }
