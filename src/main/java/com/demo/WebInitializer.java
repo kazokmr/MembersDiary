2836b85c3a5a647dbd369164ca1fc0cfe0437dd4
Kazuhiko Okumura <engkokmr@gmail.com>
2019-02-26 06:45:21 +0900

    WebInitializerの役割についてJavaDocに記載

--- src/main/java/com/demo/WebInitializer.java
+++ src/main/java/com/demo/WebInitializer.java
@@ -6,12 +6,17 @@ import com.demo.config.transaction.TransactionManagerConfig;
 import com.demo.config.web.MvcConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+/**
+DispatcherServletをサーブレットコンテナに登録する処理
+ */
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
   @Override
   protected Class<?>[] getRootConfigClasses() {
     return null;
   }
   
+  // DispatcherServlet用のアプリケーションコンテキストとするコンポーネントをセットする
+  // TODO DaoやServiceなどのアプリケーション用のコンポーネントはサーブレットリスナーにセットする方がいいとは思うが。。。
   @Override
   protected Class<?>[] getServletConfigClasses() {
     return new Class[]{
