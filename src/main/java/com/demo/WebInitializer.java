85f567079a58171aa8549b5b3ee4849df1037858
Kazuhiko Okumura <engkokmr@gmail.com>
2019-03-22 01:12:12 +0900

    - JDKを12に変更(PMDはまだ未対応なので11でチェック) - WebInitializerを見直してweb.xmlでの正しい定義に合わせてみた - web.xmlのCharacterEncodingFilterをコメントアウト(WebInitializerで定義したため)

--- src/main/java/com/demo/WebInitializer.java
+++ src/main/java/com/demo/WebInitializer.java
@@ -4,31 +4,74 @@ import com.demo.config.jdbc.DatasourceEmbeddedConfig;
 import com.demo.config.jdbc.JdbcConfig;
 import com.demo.config.transaction.TransactionManagerConfig;
 import com.demo.config.web.MvcConfig;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.Filter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
-DispatcherServletをサーブレットコンテナに登録する処理
+ * Servletのセットアップ処理. `web.xml`におけるListenerとServlet定義をこのClassで宣言します
  */
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+  /**
+   * ContextLoaderListenerのセットアップ.
+   * `web.xml`だとListenerタグでContextLoaderListerクラスを定義し、Webアプリ用のBeanとしてSpring MVCに依存しないコンポーネントのConfigurationクラスをセットします.
+   * (主にService, Repository, DataSourceやドメインロジックなどSpring MVCから直接参照しないクラスが対象.)
+   *
+   * @return Configuration Classes for Web Application.
+   */
   @Override
   protected Class<?>[] getRootConfigClasses() {
-    return null;
-  }
-  
-  // DispatcherServlet用のアプリケーションコンテキストとするコンポーネントをセットする
-  // TODO DaoやServiceなどのアプリケーション用のコンポーネントはサーブレットリスナーにセットする方がいいとは思うが。。。
-  @Override
-  protected Class<?>[] getServletConfigClasses() {
     return new Class[]{
         DatasourceEmbeddedConfig.class,
         JdbcConfig.class,
-        TransactionManagerConfig.class,
+        TransactionManagerConfig.class
+    };
+  }
+  
+  /**
+   * DispatcherServletのセットアップ.
+   * `web.xml`だとservletタグでDispatcherServletクラスを定義し、Spring MVC用のコンポーネントをConfigurationクラスをセットします.
+   * (主に\@EnableWebMVCを付けたConfigurationクラスが対象.)
+   * DispatcherServletはSpring MVCにおけるフロントエンドコントローラーのことでここからSpring MVCのコントローラークラスのメソッドを呼び出します.
+   *
+   * @return Configuration Classes for Dispatcher Servlet.
+   */
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[]{
         MvcConfig.class
     };
   }
   
+  /**
+   * ServletにマッピングするURLパターンのセットアップ.
+   * `web.xml`だとservlet-mappingタグに相当し、servlet-nameに紐づいてリクエストをハンドリングするURLパターンを指定します.
+   *
+   * @return URL Pattern
+   */
   @Override
   protected String[] getServletMappings() {
     return new String[]{"/"};
+  }
+  
+  /**
+   * Servlet Filterの登録
+   * `web.xml`だとfilterタグに相当し、フィルタークラスを指定します.
+   * 今回は日本語の文字化け対応としてCharacterEncodingFilterを追加し、文字エンコードをUTF-8にします.
+   * (filter-mappingはonStartUpメソッドをオーバーライドして定義する必要があるがCharacterEncodeingFilterは全てのリクエストを対象にするので指定しない)
+   *
+   * @return Servlet Filters
+   */
+  @Override
+  protected Filter[] getServletFilters() {
+    
+    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+    characterEncodingFilter.setEncoding(StandardCharsets.UTF_8.name());
+    characterEncodingFilter.setForceEncoding(true);
+    
+    return new Filter[]{
+        characterEncodingFilter
+    };
   }
 }
